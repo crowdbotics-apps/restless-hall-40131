38de022908e71d53fdbeb9211b9ae3e69406ad24
Crowdbotics <team@crowdbotics.com>
2023-04-23 05:55:21 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "restless-hall-40131",
+  url: "https://restless-hall-40131.botics.co",
+  api: "https://restless-hall-40131.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
